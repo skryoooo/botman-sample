06b37b2d1ab3086bfe72e83c4bce3ec2f4bc47af
Ryosuke Kawahara <ryosuke-kawahara@dac.co.jp>
2018-08-21 10:57:53 +0900

    no message

--- routes/botman.php
+++ routes/botman.php
@@ -3,11 +3,11 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('@kawabot Hi', function ($bot) {
+$botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 
-$botman->hears('@UBD1ASJ84 ping', function ($bot) {
+$botman->hears('ping', function ($bot) {
     $bot->reply('pong');
 });
 
